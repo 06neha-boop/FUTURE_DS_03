2c68ecf62514fc95fd44583b12ac74450e0b354a
06neha-boop <06parmarneha@gmail.com>
2026-04-17 01:23:27 +0530

    Task 3.py

--- Task 3.py
+++ Task 3.py
@@ -1,40 +1,25 @@
 import matplotlib.pyplot as plt
 
-# Funnel data
+# Simple Data
 stages = ["Awareness", "Interest", "Consideration", "Purchase"]
 users = [1000, 700, 400, 150]
 
-# -------------------------
-#  PIE CHART
-# -------------------------
+# -------- Basic Info --------
+print("Total Users:", sum(users))
+print("Average Users:", sum(users)/len(users))
+print("Highest Users:", max(users))
+print("Lowest Users:", min(users))
+
+# -------- Graph 1: Bar Chart --------
 plt.figure()
-plt.pie(users, labels=stages, autopct='%1.1f%%')
-plt.title("User Distribution in Marketing Funnel")
+plt.bar(stages, users)
+plt.title("Users in Marketing Funnel")
+plt.xlabel("Stages")
+plt.ylabel("Number of Users")
 plt.show()
 
-# -------------------------
-#  STATISTICAL ANALYSIS
-# -------------------------
-
-# Total users
-total_users = sum(users)
-
-# Average users
-avg_users = total_users / len(users)
-
-# Maximum and minimum
-max_users = max(users)
-min_users = min(users)
-
-# Drop-off calculation
-drop_off = []
-for i in range(1, len(users)):
-    drop_off.append(users[i-1] - users[i])
-
-# Print results
-print("\n Statistical Analysis:")
-print("Total Users:", total_users)
-print("Average Users:", avg_users)
-print("Highest Users:", max_users)
-print("Lowest Users:", min_users)
-print("Drop-offs between stages:", drop_off)
+# -------- Graph 2: Pie Chart --------
+plt.figure()
+plt.pie(users, labels=stages)
+plt.title("User Distribution")
+plt.show()
